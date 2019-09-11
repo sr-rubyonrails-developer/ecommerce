aec225f3063c688720899bc82620c1d84b16ada2
loco <leinadlm95@gmail.com>
2019-09-11 17:49:05 -0500

    Quité clases innecesarias de CSS

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :read, :all
+      can :read, Product
     end
     #
     # The first argument to `can` is the action you are giving the user

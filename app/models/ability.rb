8f6a51bb86c80e6eca1da244f4ee96506c73f737
loco <leinadlm95@gmail.com>
2019-09-12 11:06:15 -0500

    Puse el header como partial

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,8 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
+    # alias_action :create, :read, :update, :destroy, to: :crud
+
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
